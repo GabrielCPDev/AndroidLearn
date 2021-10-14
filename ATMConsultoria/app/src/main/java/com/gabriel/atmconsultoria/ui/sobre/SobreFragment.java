e947c6057d38b8c26909a0cde060020c8a39ec61
GabrielCPDev <gabrielcpcontato@outlook.com>
2021-10-14 17:29:09 -0300

    trabalhando com bibliotecas externas para cricao de fragment Sobre

--- ATMConsultoria/app/src/main/java/com/gabriel/atmconsultoria/ui/sobre/SobreFragment.java
+++ ATMConsultoria/app/src/main/java/com/gabriel/atmconsultoria/ui/sobre/SobreFragment.java
@@ -10,57 +10,42 @@ import android.view.ViewGroup;
 
 import com.gabriel.atmconsultoria.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SobreFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import mehdi.sakout.aboutpage.AboutPage;
+import mehdi.sakout.aboutpage.Element;
+
 public class SobreFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     public SobreFragment() {
-        // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SobreFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SobreFragment newInstance(String param1, String param2) {
-        SobreFragment fragment = new SobreFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sobre, container, false);
+
+        String descricao = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+
+        Element versao = new Element();
+        versao.setTitle("Versão 1.0.1");
+
+        return new AboutPage(getActivity())
+                .setImage(R.drawable.logo)
+                .setDescription(descricao)
+                .addGroup("Entre em contato")
+                .addEmail("email@gmail.com","Envie um email")
+                .addWebsite("www.atm.com.br", "Acesse nosso site")
+                .addGroup("Redes Sociais")
+                .addFacebook("usuario", "Facebook")
+                .addInstagram("usuario", "Instagram")
+                .addTwitter("uruario", "Twiter")
+                .addItem(versao)
+                .create();
+
+        //return inflater.inflate(R.layout.fragment_sobre, container, false);
     }
 }
