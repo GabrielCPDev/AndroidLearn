ff9400570b2c8692b7689ee46da553ba16a669ff
GabrielCPDev <gabrielcpcontato@outlook.com>
2021-10-14 11:34:00 -0300

    Trabalhando com fragments e Navigation Drawer

--- NavigationDrawer/app/src/main/java/com/gabriel/navigationdrawer/MainActivity.java
+++ NavigationDrawer/app/src/main/java/com/gabriel/navigationdrawer/MainActivity.java
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
 
 
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.nav_contato)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
