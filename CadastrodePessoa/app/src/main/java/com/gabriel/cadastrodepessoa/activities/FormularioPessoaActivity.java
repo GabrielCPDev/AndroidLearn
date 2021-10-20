189f0fb6967b8474ed6ba02b84d9de8c0b2b5712
GabrielCPDev <gabrielcpcontato@outlook.com>
2021-10-20 11:37:56 -0300

    refatorando codigo

--- CadastrodePessoa/app/src/main/java/com/gabriel/cadastrodepessoa/activities/FormularioPessoaActivity.java
+++ CadastrodePessoa/app/src/main/java/com/gabriel/cadastrodepessoa/activities/FormularioPessoaActivity.java
@@ -1,13 +1,11 @@
 package com.gabriel.cadastrodepessoa.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.gabriel.cadastrodepessoa.R;
 import com.gabriel.cadastrodepessoa.dao.PessoaDAO;
@@ -21,82 +19,90 @@ import java.util.Map;
 
 public class FormularioPessoaActivity extends AppCompatActivity {
 
+    public static final String NOVA_PESSOA = "Nova Pessoa";
+
     private Button botaoSalvar;
     private EditText nome, idade, endereco;
     private String mensagemDeErro;
+    private PessoaDAO dao = new PessoaDAO();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario_pessoa);
+        setTitle(NOVA_PESSOA);
 
         PessoaDAO dao = new PessoaDAO();
+        inicializaCampos();
+        configuraBotaoSalvar();
+    }
+
+    private void inicializaCampos() {
         botaoSalvar = findViewById(R.id.botao_cadastrar);
         nome = findViewById(R.id.activity_formulario_pessoa_nome);
         endereco = findViewById(R.id.activity_formulario_pessoa_endereco);
         idade = findViewById(R.id.activity_formulario_pessoa_idade);
-
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.R) {
-            botaoSalvar.setOnClickListener(new View.OnClickListener() {
-
-
-                @Override
-                public void onClick(View view) {
-                    if(validaDados(nome, endereco, idade)){
-                        Map<String, String> camposPessoa = new HashMap<>();
-                        camposPessoa.put("nome", nome.getText().toString());
-                        camposPessoa.put("endereco",endereco.getText().toString());
-                        camposPessoa.put("idade", idade.getText().toString());
-
-                        Pessoa pessoa = constroePessoa(camposPessoa);
-                        dao.salva(pessoa);
-
-                        //startActivity(new Intent(FormularioPessoaActivity.this, MainActivity.class));
-                        finish();
-                    }else{
-                        Snackbar.make(view, mensagemDeErro, Snackbar.LENGTH_LONG).show();
-                    }
-
-                }
-            });
-        }
-
     }
 
-    private Pessoa constroePessoa(Map<String , String> campos){
+    private void configuraBotaoSalvar() {
+        botaoSalvar.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                if (validaDados(nome, endereco, idade)) {
+
+                    Pessoa pessoa = constroePessoa();
+                    salvaPessoa(pessoa);
+
+                } else {
+                    Snackbar.make(view, mensagemDeErro, Snackbar.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    private void salvaPessoa(Pessoa pessoa) {
+        dao.salva(pessoa);
+        //startActivity(new Intent(FormularioPessoaActivity.this, MainActivity.class));
+        finish();
+    }
+
+    private Pessoa constroePessoa() {
         Pessoa pessoa = new Pessoa();
-        pessoa.setNome(campos.get("nome").toString());
-        pessoa.setEnderedo(campos.get("endereco").toString());
-        pessoa.setIdade((Integer.parseInt(campos.get("idade").toString())) );
+        pessoa.setNome(nome.getText().toString());
+        pessoa.setEnderedo(endereco.getText().toString());
+        pessoa.setIdade((Integer.parseInt(idade.getText().toString())));
 
         return pessoa;
     }
 
-    private boolean validaDados(EditText nome, EditText endereco, EditText idade){
+    private boolean validaDados(EditText nome, EditText endereco, EditText idade) {
         List<String> erros = new ArrayList<>();
 
-        if(nome.getText().toString().equals("") || nome.getText().toString().equals(null)){
+        if (nome.getText().toString().equals("") || nome.getText().toString().equals(null)) {
             erros.add("nome");
-        }if(endereco.getText().toString().equals("") || endereco.getText().toString().equals(null)){
+        }
+        if (endereco.getText().toString().equals("") || endereco.getText().toString().equals(null)) {
             erros.add("endereco");
-        }if(idade.getText().toString().equals("") || idade.getText().toString().equals(null)){
+        }
+        if (idade.getText().toString().equals("") || idade.getText().toString().equals(null)) {
             erros.add("idade");
         }
 
-        if (!erros.isEmpty()){
+        if (!erros.isEmpty()) {
             criaMensagemErro(erros);
         }
         return true;
     }
 
-    public void criaMensagemErro(List<String> campos){
+    public void criaMensagemErro(List<String> campos) {
         String mensage = "Preenchimento de campo: ";
-        for (String i : campos){
+        for (String i : campos) {
             mensage += ", " + i;
         }
-       mensage += " é obrigatório";
+        mensage += " é obrigatório";
 
-       this.mensagemDeErro = mensage;
+        this.mensagemDeErro = mensage;
     }
 
 }
--- CadastrodePessoa/app/src/main/java/com/gabriel/cadastrodepessoa/activities/MainActivity.java
+++ CadastrodePessoa/app/src/main/java/com/gabriel/cadastrodepessoa/activities/MainActivity.java
@@ -1,8 +1,5 @@
 package com.gabriel.cadastrodepessoa.activities;
 
-import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
@@ -10,43 +7,55 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.gabriel.cadastrodepessoa.R;
 import com.gabriel.cadastrodepessoa.dao.PessoaDAO;
 import com.gabriel.cadastrodepessoa.entities.Pessoa;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class MainActivity extends AppCompatActivity {
 
     private ListView listView;
     private FloatingActionButton fab;
+    private PessoaDAO dao = new PessoaDAO();
 
     @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        configuraFabNovaPessoa();
+    }
+
+    private void configuraFabNovaPessoa() {
         fab = findViewById(R.id.fab);
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, FormularioPessoaActivity.class));
+                abreFormularioPessoaActivity();
             }
         });
+    }
+
+    private void abreFormularioPessoaActivity() {
+        startActivity(new Intent(this, FormularioPessoaActivity.class));
+    }
+
+    private void configuraLista() {
+        listView = findViewById(R.id.listPessoa);
+        List<Pessoa> pessoas = dao.listaPessoas();
+        listView.setAdapter(new ArrayAdapter<Pessoa>(this, android.R.layout.simple_list_item_1, pessoas));
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        PessoaDAO dao = new PessoaDAO();
-        listView = findViewById(R.id.listPessoa);
+        configuraLista();
 
-        List<Pessoa> pessoas = dao.listaPessoas();
-        listView.setAdapter(new ArrayAdapter<Pessoa>(this, android.R.layout.simple_list_item_1, pessoas));
     }
 }
