612c78449a180761889c615dea6e9c3b09fa4da8
GabrielCPDev <gabrielcpcontato@outlook.com>
2021-11-01 14:13:27 -0300

    Criando projeto, cardView, Adapter customizado e binding de imagens no Drawable

---
+++ AluraViagens/app/src/main/java/com/example/aluraviagens/ui/activity/ListaPacotesActivity.java
@@ -0,0 +1,27 @@
+package com.example.aluraviagens.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ListView;
+
+import com.example.aluraviagens.R;
+import com.example.aluraviagens.ui.activity.adapters.ListaPacotesAdapter;
+import com.example.aluraviagens.ui.dao.PacoteDAO;
+import com.example.aluraviagens.ui.models.Pacote;
+
+import java.util.List;
+
+public class ListaPacotesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_pacotes);
+        ListView listaDePacotes = findViewById(R.id.lista_pacotes_listview);
+
+        List<Pacote> pacotes = new PacoteDAO().lista();
+
+        listaDePacotes.setAdapter(new ListaPacotesAdapter(pacotes, this));
+    }
+}
