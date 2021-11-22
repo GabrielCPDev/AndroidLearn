17e61f36033c8dce2085aacd5ad08a14e8362c26
GabrielCPDev <gabrielcpcontato@outlook.com>
2021-11-22 16:16:07 -0300

    refatorando codigo

--- ListaDeAlunos/app/src/main/java/br/com/alura/agenda/database/AgendaDatabase.java
+++ ListaDeAlunos/app/src/main/java/br/com/alura/agenda/database/AgendaDatabase.java
@@ -1,6 +1,9 @@
 package br.com.alura.agenda.database;
 
+import android.content.Context;
+
 import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
 import br.com.alura.agenda.database.dao.RoomAlunoDAO;
@@ -9,6 +12,12 @@ import br.com.alura.agenda.model.Aluno;
 @Database(entities = {Aluno.class},version = 1, exportSchema = false)
 public abstract class AgendaDatabase extends RoomDatabase {
 
+    private static final String NOME_BANCO_DE_DADOS = "agenda.db";
+
     public abstract RoomAlunoDAO getRoomAlunoDAO();
+
+    public static AgendaDatabase getInstance(Context context){
+        return Room.databaseBuilder(context, AgendaDatabase.class, NOME_BANCO_DE_DADOS).allowMainThreadQueries().build();
+    }
 
 }
