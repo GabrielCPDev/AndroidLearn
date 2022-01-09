317b20e12b0e76035cd864dff4f3bad78a3a6e18
GabrielCPDev <gabrielcpcontato@outlook.com>
2022-01-09 16:24:12 -0300

    criando menu de opções

--- Menus/app/src/main/java/com/example/menus/MainActivity.java
+++ Menus/app/src/main/java/com/example/menus/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.menus;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_principal, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.itemSalvar:
+                Toast.makeText(MainActivity.this, "ItemSalvar", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.itemEditar:
+                Toast.makeText(MainActivity.this, "ItemEditar", Toast.LENGTH_LONG).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
