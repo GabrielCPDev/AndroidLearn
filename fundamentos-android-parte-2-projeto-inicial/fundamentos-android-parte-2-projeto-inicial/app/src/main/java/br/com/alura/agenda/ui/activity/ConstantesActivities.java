2ec644c309b7adec456e4c0abee0535da7e47de5
GabrielCPDev <gabrielcpcontato@outlook.com>
2022-01-30 17:02:23 -0300

    excluindo projetos repetidos

--- fundamentos-android-parte-2-projeto-inicial/fundamentos-android-parte-2-projeto-inicial/app/src/main/java/br/com/alura/agenda/ui/activity/ConstantesActivities.java
+++
@@ -1,8 +0,0 @@
-package br.com.alura.agenda.ui.activity;
-
-public interface ConstantesActivities {
-
-    String TITULO_APPBAR_EDITA_ALUNO = "Edita Aluno";
-    String TITULO_APPBAR_NOVO_ALUNO = "Lista de alunos";
-    String CHAVE_ALUNO = "aluno";
-}
