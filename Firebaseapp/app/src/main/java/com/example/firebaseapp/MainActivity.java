7a0fbca107a7fa15bd27ddab8ff689cb20929d52
GabrielCPDev <gabrielcpcontato@outlook.com>
2022-02-17 11:21:23 -0300

    implementando instancia de dataqbase e salvando dados no firebase

--- Firebaseapp/app/src/main/java/com/example/firebaseapp/MainActivity.java
+++ Firebaseapp/app/src/main/java/com/example/firebaseapp/MainActivity.java
@@ -4,11 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
+
+    private DatabaseReference referencia = FirebaseDatabase.getInstance().getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DatabaseReference usuarios = referencia.child("usuarios");
+        DatabaseReference produtos = referencia.child("produtos");
+        Usuario u = new Usuario("Naruto", "Usumaki",17);
+        Produto p = new Produto("Lamen","Ichiraku", 10.0 );
+        usuarios.child("002").setValue(u);
+        produtos.child("001").setValue(p);
     }
 }
