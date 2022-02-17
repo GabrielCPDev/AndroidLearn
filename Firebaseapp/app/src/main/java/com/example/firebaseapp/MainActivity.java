ab2d7c98ee1c4bf65f9b5ef9bcf6bb79fd4db74e
GabrielCPDev <gabrielcpcontato@outlook.com>
2022-02-17 11:56:33 -0300

    recuperando dados do firebase

--- Firebaseapp/app/src/main/java/com/example/firebaseapp/MainActivity.java
+++ Firebaseapp/app/src/main/java/com/example/firebaseapp/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.firebaseapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,9 +23,21 @@ public class MainActivity extends AppCompatActivity {
 
         DatabaseReference usuarios = referencia.child("usuarios");
         DatabaseReference produtos = referencia.child("produtos");
-        Usuario u = new Usuario("Naruto", "Usumaki",17);
-        Produto p = new Produto("Lamen","Ichiraku", 10.0 );
-        usuarios.child("002").setValue(u);
-        produtos.child("001").setValue(p);
+
+        usuarios.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Log.i("FIREBASE", snapshot.getValue().toString());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+//        Usuario u = new Usuario("Naruto", "Usumaki",17);
+//        Produto p = new Produto("Lamen","Ichiraku", 10.0 );
+//        usuarios.child("002").setValue(u);
+//        produtos.child("001").setValue(p);
     }
 }
