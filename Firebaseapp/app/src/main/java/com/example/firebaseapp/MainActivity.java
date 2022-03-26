d181737ff529139f1665fddbb1229e86d3617874
GabrielCPDev <gabrielcpcontato@outlook.com>
2022-03-26 16:31:21 -0300

    adicionando login por email e verificando se usuario esta logado

--- Firebaseapp/app/src/main/java/com/example/firebaseapp/MainActivity.java
+++ Firebaseapp/app/src/main/java/com/example/firebaseapp/MainActivity.java
@@ -6,6 +6,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -15,7 +20,7 @@ import com.google.firebase.database.ValueEventListener;
 public class MainActivity extends AppCompatActivity {
 
     private DatabaseReference referencia = FirebaseDatabase.getInstance().getReference();
-
+    private FirebaseAuth auth = FirebaseAuth.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,20 +29,48 @@ public class MainActivity extends AppCompatActivity {
         DatabaseReference usuarios = referencia.child("usuarios");
         DatabaseReference produtos = referencia.child("produtos");
 
-        usuarios.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                Log.i("FIREBASE", snapshot.getValue().toString());
-            }
+        //Cadastrando usuario
+//        auth.createUserWithEmailAndPassword(
+//                "gabrielcpcontato@gmailc.com",
+//                "123456")
+//                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<AuthResult> task) {
+//                        if(task.isSuccessful()){
+//                            Log.i("SUCESSSO", "onComplete: ");
+//                        }
+//                        else {
+//                            Log.i("ERRO", "cadastro nao sucedido: ");
+//
+//                        }
+//                    }
+//                });
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
+        //Verificando usuario logado
+        FirebaseUser usuarioLogado = auth.getCurrentUser();
+        if(usuarioLogado != null){
+            Log.i("SUCESSO", "usuario logado");
+        }else {
+            Log.i("FALHA ", "usuario nao logado");
 
-            }
-        });
+        }
+
 //        Usuario u = new Usuario("Naruto", "Usumaki",17);
 //        Produto p = new Produto("Lamen","Ichiraku", 10.0 );
 //        usuarios.child("002").setValue(u);
 //        produtos.child("001").setValue(p);
+
+//        usuarios.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                Log.i("FIREBASE", snapshot.getValue().toString());
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError error) {
+//
+//            }
+//        });
+
     }
 }
