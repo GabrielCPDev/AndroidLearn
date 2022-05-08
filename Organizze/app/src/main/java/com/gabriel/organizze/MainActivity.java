daf51d739cb97d13449d7e9afb5fa636fa36951c
GabrielCPDev <gabrielcpcontato@outlook.com>
2022-05-08 16:48:26 -0300

    wip

---
+++ Organizze/app/src/main/java/com/gabriel/organizze/MainActivity.java
@@ -0,0 +1,27 @@
+package com.gabriel.organizze;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.gabriel.organizze.activities.CadastroActivity;
+import com.gabriel.organizze.activities.LoginActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void entrar(View view){
+        startActivity(new Intent(this, LoginActivity.class));
+
+    }
+    public void cadastrar(View view){
+        startActivity(new Intent(this, CadastroActivity.class));
+    }
+}
