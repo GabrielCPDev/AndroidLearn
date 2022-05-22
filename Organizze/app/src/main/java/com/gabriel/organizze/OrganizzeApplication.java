54b61e10d89473a3c4cd4418aebfdbd32838672c
GabrielCPDev <gabrielcpcontato@outlook.com>
2022-05-22 16:12:51 -0300

    criando classe Application do projeto

---
+++ Organizze/app/src/main/java/com/gabriel/organizze/OrganizzeApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel.organizze;
+
+import android.app.Application;
+
+import com.google.firebase.FirebaseApp;
+
+public class OrganizzeApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
