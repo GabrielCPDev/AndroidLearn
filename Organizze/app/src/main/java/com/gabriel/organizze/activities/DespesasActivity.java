5d00c8377e7ce4032772102e86bd72ab200b2cf2
GabrielCPDev <gabrielcpcontato@outlook.com>
2022-06-05 16:27:22 -0300

    adicionando sugestão de data atual em InputText de data em DespesasActivity.java e ReceitasActivity.java

--- Organizze/app/src/main/java/com/gabriel/organizze/activities/DespesasActivity.java
+++ Organizze/app/src/main/java/com/gabriel/organizze/activities/DespesasActivity.java
@@ -1,19 +1,29 @@
 package com.gabriel.organizze.activities;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
 
 import com.gabriel.organizze.databinding.ActivityDespesasBinding;
+import com.gabriel.organizze.helper.DateUtil;
 
 public class DespesasActivity extends AppCompatActivity {
 
     private ActivityDespesasBinding binding;
 
     @Override
+    @RequiresApi(api = Build.VERSION_CODES.O)
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityDespesasBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        setDataAtual();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void setDataAtual() {
+        binding.editData.setText(DateUtil.dataAtual());
     }
 }
--- Organizze/app/src/main/java/com/gabriel/organizze/activities/ReceitasActivity.java
+++ Organizze/app/src/main/java/com/gabriel/organizze/activities/ReceitasActivity.java
@@ -1,18 +1,30 @@
 package com.gabriel.organizze.activities;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
 
 import com.gabriel.organizze.databinding.ActivityReceitasBinding;
+import com.gabriel.organizze.helper.DateUtil;
+import com.google.android.gms.common.util.DataUtils;
 
 public class ReceitasActivity extends AppCompatActivity {
 
     private ActivityReceitasBinding bindig;
+
     @Override
+    @RequiresApi(api = Build.VERSION_CODES.O)
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         bindig = ActivityReceitasBinding.inflate(getLayoutInflater());
         setContentView(bindig.getRoot());
+        setDataAtual();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void setDataAtual() {
+        bindig.editData.setText(DateUtil.dataAtual());
     }
 }
