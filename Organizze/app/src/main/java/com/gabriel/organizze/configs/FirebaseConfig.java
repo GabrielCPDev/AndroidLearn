ea07cc26c48e8dbdb9bd2cf0671e9d7b3a0b5696
GabrielCPDev <gabrielcpcontato@outlook.com>
2022-06-05 11:54:51 -0300

    criando método que retorna instancia de um DatabaseReference do FirebaseConfig

--- Organizze/app/src/main/java/com/gabriel/organizze/configs/FirebaseConfig.java
+++ Organizze/app/src/main/java/com/gabriel/organizze/configs/FirebaseConfig.java
@@ -4,10 +4,14 @@ import android.content.Context;
 
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class FirebaseConfig {
 
     private static FirebaseAuth autenticacao;
+    private static DatabaseReference firebase;
 
     public static FirebaseAuth getFirebaseAutenticacao(){
         if(autenticacao == null){
@@ -15,5 +19,12 @@ public class FirebaseConfig {
             autenticacao = FirebaseAuth.getInstance();
         }
         return autenticacao;
+    }
+
+    public static DatabaseReference getFirebaseDatabase(){
+        if (firebase == null){
+            firebase = FirebaseDatabase.getInstance().getReference();
+        }
+        return firebase;
     }
 }
