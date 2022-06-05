766700fdc5b3f14106978a98c71dbf815adf0791
GabrielCPDev <gabrielcpcontato@outlook.com>
2022-06-05 11:55:37 -0300

    criando método que salva usuaio no realtime database em Usuario.java

--- Organizze/app/src/main/java/com/gabriel/organizze/models/Usuario.java
+++ Organizze/app/src/main/java/com/gabriel/organizze/models/Usuario.java
@@ -1,7 +1,13 @@
 package com.gabriel.organizze.models;
 
+import com.gabriel.organizze.configs.FirebaseConfig;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Exclude;
+
 public class Usuario {
 
+    private final String node = "usuarios";
+    private String idUsuario;
     private String nome;
     private String email;
     private String senha;
@@ -9,12 +15,17 @@ public class Usuario {
     public Usuario(){
 
     }
-
-    public Usuario(String nome, String email, String senha) {
+    public Usuario(String idUsuario, String nome, String email, String senha) {
+        this.idUsuario = idUsuario;
         this.nome = nome;
         this.email = email;
         this.senha = senha;
     }
+
+    @Exclude
+    public String getIdUsuario() { return idUsuario; }
+
+    public void setIdUsuario(String idUsuario) { this.idUsuario = idUsuario; }
 
     public String getNome() {
         return nome;
@@ -32,11 +43,17 @@ public class Usuario {
         this.email = email;
     }
 
+    @Exclude
     public String getSenha() {
         return senha;
     }
 
     public void setSenha(String senha) {
         this.senha = senha;
+    }
+
+    public void salvar (){
+        DatabaseReference firebase = FirebaseConfig.getFirebaseDatabase();
+        firebase.child(node).child(this.idUsuario).setValue(this);
     }
 }
