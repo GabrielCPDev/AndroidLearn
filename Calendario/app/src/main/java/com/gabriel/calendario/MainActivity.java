a78ebc8dd3f07646251024e8df5805124d3c97f4
GabrielCPDev <gabrielcpcontato@outlook.com>
2022-06-10 10:47:40 -0300

    trabalhando com calendario

---
+++ Calendario/app/src/main/java/com/gabriel/calendario/MainActivity.java
@@ -0,0 +1,26 @@
+package com.gabriel.calendario;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.CalendarView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private CalendarView calendarView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        calendarView = findViewById(R.id.calendarView);
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                Log.i("CALENDARIO", "Hoje é: " + dayOfMonth + " de " + month + " de " + year);
+            }
+        });
+    }
+}
