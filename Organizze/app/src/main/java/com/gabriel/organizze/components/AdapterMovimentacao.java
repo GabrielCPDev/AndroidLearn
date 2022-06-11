44354bd303c6d65a1c500fca243933ba477f43dd
GabrielCPDev <gabrielcpcontato@outlook.com>
2022-06-11 20:01:04 -0300

    criando Adapter e Holder para RecyclerView

---
+++ Organizze/app/src/main/java/com/gabriel/organizze/components/AdapterMovimentacao.java
@@ -0,0 +1,65 @@
+package com.gabriel.organizze.components;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.gabriel.organizze.R;
+import com.gabriel.organizze.models.Movimentacao;
+import java.util.List;
+
+public class AdapterMovimentacao extends RecyclerView.Adapter<AdapterMovimentacao.MovimentacaoViewHolder> {
+
+    List<Movimentacao> movimentacoes;
+    Context context;
+
+    public AdapterMovimentacao(List<Movimentacao> movimentacoes, Context context) {
+        this.movimentacoes = movimentacoes;
+        this.context = context;
+    }
+
+    @Override
+    public MovimentacaoViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemLista = LayoutInflater.from(parent.getContext()).inflate(R.layout.adapter_movimentacao, parent, false);
+        return new MovimentacaoViewHolder(itemLista);
+    }
+
+
+    @Override
+    public void onBindViewHolder(MovimentacaoViewHolder holder, int position) {
+        Movimentacao movimentacao = movimentacoes.get(position);
+
+        holder.titulo.setText(movimentacao.getDescricao());
+        holder.valor.setText(String.valueOf(movimentacao.getValor()));
+        holder.categoria.setText(movimentacao.getCategoria());
+
+        if (movimentacao.getTipo() == "d" || movimentacao.getTipo().equals("d")) {
+            holder.valor.setTextColor(context.getResources().getColor(R.color.colorPrimaryDarkDespesa));
+            holder.valor.setText("-" + movimentacao.getValor());
+        }
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return movimentacoes.size();
+    }
+
+    public class MovimentacaoViewHolder extends RecyclerView.ViewHolder {
+
+        TextView titulo, valor, categoria;
+
+        public MovimentacaoViewHolder(View itemView) {
+            super(itemView);
+
+            titulo = itemView.findViewById(R.id.textAdapterTitulo);
+            valor = itemView.findViewById(R.id.textAdapterValor);
+            categoria = itemView.findViewById(R.id.textAdapterCategoria);
+        }
+
+    }
+
+}
