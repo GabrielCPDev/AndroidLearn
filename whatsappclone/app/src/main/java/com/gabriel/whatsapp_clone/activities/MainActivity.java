6e2ef676c0f10afa3b006ef63e55a49fb8826c0b
GabrielCPDev <gabrielcpcontato@outlook.com>
2022-07-09 16:03:47 -0300

    centralizando todas as dimenções dos layouts em arquivo dimens.xml

--- whatsappclone/app/src/main/java/com/gabriel/whatsapp_clone/activities/MainActivity.java
+++ whatsappclone/app/src/main/java/com/gabriel/whatsapp_clone/activities/MainActivity.java
@@ -1,16 +1,24 @@
 package com.gabriel.whatsapp_clone.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 import com.gabriel.whatsapp_clone.R;
+import com.gabriel.whatsapp_clone.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        setSupportActionBar(toolbar);
     }
 }
