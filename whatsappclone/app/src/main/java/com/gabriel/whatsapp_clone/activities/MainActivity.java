c6eaf478286ffa81234556347b3b37b32fa65708
GabrielCPDev <gabrielcpcontato@outlook.com>
2022-07-10 14:39:04 -0300

    adicionando toolbar e implementando metodo deslogar

--- whatsappclone/app/src/main/java/com/gabriel/whatsapp_clone/activities/MainActivity.java
+++ whatsappclone/app/src/main/java/com/gabriel/whatsapp_clone/activities/MainActivity.java
@@ -1,22 +1,64 @@
 package com.gabriel.whatsapp_clone.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.res.ColorStateList;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.gabriel.whatsapp_clone.R;
+import com.gabriel.whatsapp_clone.configuration.FirebaseConfig;
 import com.gabriel.whatsapp_clone.databinding.ActivityMainBinding;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
     private Toolbar toolbar;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        auth = FirebaseConfig.getFirebaseAuth();
+        configToolbar();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menuSair:
+                signoutUser();
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void signoutUser() {
+        try {
+            auth.signOut();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private void configToolbar() {
         toolbar = findViewById(R.id.toolbar);
         toolbar.setTitle(R.string.app_name);
         setSupportActionBar(toolbar);
