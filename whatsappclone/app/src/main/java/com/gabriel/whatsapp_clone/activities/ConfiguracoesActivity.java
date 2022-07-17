b8b5616201a6798502c3bf8e6159f388e08854c1
GabrielCPDev <gabrielcpcontato@outlook.com>
2022-07-17 18:25:24 -0300

    recuperando imagem de camera ou galeria e setando no perfil

--- whatsappclone/app/src/main/java/com/gabriel/whatsapp_clone/activities/ConfiguracoesActivity.java
+++ whatsappclone/app/src/main/java/com/gabriel/whatsapp_clone/activities/ConfiguracoesActivity.java
@@ -1,6 +1,7 @@
 package com.gabriel.whatsapp_clone.activities;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AlertDialog;
@@ -9,10 +10,15 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.Manifest;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.ContextThemeWrapper;
+import android.view.View;
 
 import com.gabriel.whatsapp_clone.R;
 import com.gabriel.whatsapp_clone.databinding.ActivityConfiguracoesBinding;
@@ -23,6 +29,8 @@ import java.util.List;
 
 public class ConfiguracoesActivity extends AppCompatActivity {
 
+    private static final int SELECAO_CAMERA = 100;
+    private static final int SELECAO_GALERIA = 200;
     private ActivityConfiguracoesBinding binding;
     private Toolbar toolbar;
     private List<String> permissoesNecessarias =
@@ -37,7 +45,50 @@ public class ConfiguracoesActivity extends AppCompatActivity {
         binding = ActivityConfiguracoesBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         configToolbar();
-        boolean temPermissoes = Permissao.validarPermissoes(this, permissoesNecessarias, 1);
+        configClikCameraOuGaleria();
+        Permissao.validarPermissoes(this, permissoesNecessarias, 1);
+
+    }
+
+    private void configClikCameraOuGaleria() {
+        binding.btnCamera.setOnClickListener(view -> {
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if (temPackageManager(intent)) startActivityForResult(intent, SELECAO_CAMERA);
+        });
+        binding.btnGaleria.setOnClickListener(view -> {
+            Intent intent = new Intent(Intent.ACTION_PICK,
+                                MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            if (temPackageManager(intent)) startActivityForResult(intent, SELECAO_GALERIA);
+        });
+    }
+    private boolean temPackageManager(Intent i){
+        return i.resolveActivity(getPackageManager()) != null  ? true : false;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK){
+            Bitmap imagem = null;
+            try {
+                switch (requestCode){
+                    case SELECAO_CAMERA:
+                        imagem = (Bitmap) data.getExtras().get("data");
+                        if(imagem != null) setImageOnProfile(imagem);
+                        break;
+                    case SELECAO_GALERIA:
+                        Uri localImagemSelecionada = data.getData();
+                        imagem = MediaStore.Images.Media.getBitmap(getContentResolver(), localImagemSelecionada);
+                        if(imagem != null) setImageOnProfile(imagem);
+                        break;
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+    private void setImageOnProfile(Bitmap imageBitmap) {
+        binding.circleImageViewFotoPerfil.setImageBitmap(imageBitmap);
     }
 
     @Override
